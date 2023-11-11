047d7961204e737ea2a7af7aaf8cd8657931662c
Fabrice Gallet <fbgallet@gmail.com>
2023-11-11 14:33:15 +0100

    First commit from VSCode

--- routes/offer.js
+++ routes/offer.js
@@ -10,6 +10,13 @@ const router = express.Router();
 const Offer = require("../models/Offer");
 // const User = require("../models/User");
 
+router.get("/offers", async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.post(
   "/offer/publish",
   isAuthenticated,
